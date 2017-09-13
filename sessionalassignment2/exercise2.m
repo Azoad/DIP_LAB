d74887dbb070784b8e0387187f42ab25bc159638
Azoad Ahnaf <azoadahnaf@gmail.com>
2017-09-13 07:14:51 +0600

    final

--- sessionalassignment2/exercise2.m
+++ sessionalassignment2/exercise2.m
@@ -2,7 +2,7 @@ I = imread('E:\Third Year\3 2\DIP\DIP LAB\sessionalassignment2\spine.jpg');
 Id = im2double(I);
 In = Id;
 figure;
-subplot(3,3,2),imshow(I),title('original');
+subplot(3,3,2),imshow(I),title('Original');
 z = 4;
 for c = .5:.5:3
 [M,N]=size(Id);
@@ -20,10 +20,10 @@ c = input('Enter the value of c : ');
 r = input('Enter the value of r : ');
 [M,N]=size(Id);
 figure;
-subplot(1,2,1),imshow(I),title('original');
+subplot(1,2,1),imshow(I),title('Original');
         for x = 1:M
             for y = 1:N
                 In(x,y)=c*Id(x,y)^r;
             end
         end
-subplot(1,2,2),imshow(In),title('After');
+subplot(1,2,2),imshow(In),title('Power Law Transform');
