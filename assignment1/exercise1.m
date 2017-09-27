0e9f0461659642885cadbf365c439bcb12aba3e9
Azoad Ahnaf <azoadahnaf@gmail.com>
2017-09-27 12:15:07 +0600

    First Assignment

--- assignment1/exercise1.m
+++ assignment1/exercise1.m
@@ -1,37 +1,34 @@
 % importing the .dat file
-I = fopen('E:\Third Year\3 2\DIP\DIP LAB\assignment\crossroad.dat');
+I = fopen('crossroad.dat');
 % converting the .dat file to matlab readable image format
-I = fread(I,[580 435],'*uint8');
+I = fread(I,[580,435]);
 % rotating the image to get a nice view
 I = imrotate(I,270);
+%converting the image into uint8 format
+I = uint8(I);
 % displaying the image
-figure; 
 subplot(2,3,1),imshow(I),title('crossroad'); 
-
-% generating image with box 2 pixels
-h2 = ones(2,2)/4;
-I2 = imfilter(I,h2);
-subplot(2,3,3),imshow(I2),title('2x2');
-
-% generating image with box 4 pixels
-h4 = ones(4,4)/16;
-I4 = imfilter(I,h4);
-subplot(2,3,4),imshow(I4),title('4x4');
-
-% generating image with box 8 pixels
-h8 = ones(8,8)/64;
-I8 = imfilter(I,h8);
-subplot(2,3,5),imshow(I8),title('8x8');
-
-% generating image with box 16 pixels
-h16 = ones(16,16)/256;
-I16 = imfilter(I,h16);
-subplot(2,3,6),imshow(I16),title('16x16');
-
-% checking the structure
+% Changing the image resolution sizes with 2,4,8,16 boxes
+w = floor(580/2);
+h = floor(435/2);
+I2 = imresize(I,[h w]);
+subplot(2,3,2),imshow(I2),title('box size 2');
+w = floor(580/4);
+h = floor(435/4);
+I4 = imresize(I,[h w]);
+subplot(2,3,3),imshow(I4),title('box size 4');
+w = floor(580/8);
+h = floor(435/8);
+I8 = imresize(I,[h w]);
+subplot(2,3,5),imshow(I8),title('box size 8');
+w = floor(580/16);
+h = floor(435/16);
+I16 = imresize(I,[h w]);
+subplot(2,3,6),imshow(I16),title('box size 16');
+% Displaying the obvious structure
 figure;
-subplot(2,3,1),plot(I),title('crossroad');
-subplot(2,3,3),plot(I2),title('2x2');
-subplot(2,3,4),plot(I4),title('4x4');
-subplot(2,3,5),plot(I8),title('8x8');
-subplot(2,3,6),plot(I16),title('16x16');
+subplot(2,3,1), plot(I), title('crossroad');
+subplot(2,3,2), plot(I2), title('box size 2');
+subplot(2,3,3), plot(I4), title('box size 4');
+subplot(2,3,5), plot(I8), title('box size 8');
+subplot(2,3,6), plot(I16), title('box size 16');
