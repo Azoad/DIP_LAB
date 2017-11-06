890f04c74802c427aceb04316f0bd12b572d709a
Azoad Ahnaf <azoadahnaf@gmail.com>
2017-11-06 17:35:01 +0600

    Lab Test1

--- resessionalassignment2/Sessional3/exercise2.m
+++
@@ -1,3 +0,0 @@
-I = imread('moon.tif');
-subplot(1,2,1),imshow(I),title('Original');
-subplot(1,2,2),imshow(imfilter(I,fspecial('unsharp'))),title('Sharpened');
--- resessionalassignment2/demo/sessional32.m
+++
@@ -1,5 +0,0 @@
-I = imread('miranda.tif');
-h = fspecial('unsharp');
-sharpened = imfilter(I,h);
-subplot 121;imshow(I);title('Original'); 
-subplot 122;imshow(sharpened);title('Sharpened');
